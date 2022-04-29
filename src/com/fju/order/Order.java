3eb098c7213e3571984dadc0482eb95e3fd494dc
yenccc0 <damao455174>
2022-04-29 12:13:00 +0800

    4/29

--- src/com/fju/order/Order.java
+++ src/com/fju/order/Order.java
@@ -3,9 +3,14 @@ package com.fju.order;
 public class Order {
     int amount;
     Delivery delivery;
+    int shipFeed;
+    public int total(){
+        return shipFeed+amount;
+    }
 
     public Order(int amount, Delivery delivery) {
         this.amount = amount;
         this.delivery = delivery;
+        shipFeed=delivery.price;
     }
 }
